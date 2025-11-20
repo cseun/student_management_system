3ae29821bebee1002edaf34b529ae76d7235e6ec
sein <cseeun98@gmail.com>
2025-11-21 01:04:44 +0900

    학생 성적 정보 DTO 추가

---
+++ StudentScoreInfoRow.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include "StudentListKey.h"
+
+struct StudentScoreInfoRow
+{
+	std::string grade;
+	std::string classNumber;
+	std::string studentNumber;
+	std::string name;
+
+	std::string year;
+	std::string semester;
+	std::string examType;
+
+	std::string kukScore;
+	std::string engScore;
+	std::string mathScore;
+	std::string socialScore;
+	std::string scienceScore;
+	std::string totalScore;
+
+	std::string rank;
+
+	static std::vector<std::string> getAttributeNames()
+	{
+		return { "학년", "반", "번호", "이름", "년도", "학기", "시험종류", "국어", "영어", "수학", "사회", "과학", "총점", "등수" };
+	}
+	const std::vector<std::string> toVector() const {
+		return {
+			grade, 
+			classNumber, 
+			studentNumber, 
+			name,
+			year,
+			semester,
+			examType,
+			kukScore, 
+			engScore, 
+			mathScore,
+			socialScore,
+			scienceScore,
+			totalScore, 
+			rank
+		};
+	}
+	const StudentListKey getListKey() const
+	{
+		return StudentListKey{
+			std::stoi(grade),
+			std::stoi(classNumber),
+			std::stoi(studentNumber)
+		};
+	}
+};
