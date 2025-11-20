c69edb6c6b18329b20449932e99975177373fb8a
sein <cseeun98@gmail.com>
2025-11-20 22:00:56 +0900

    학생 객체 get,set 수정 및 리펙터링

--- student.cpp
+++ student.cpp
@@ -1,59 +1,48 @@
 ﻿#include "pch.h"
 #include <string>
 #include <sstream>
-#include <chrono>
 #include "Student.h"
 
 int Student::studentIndex = 0;  // 정적 멤버 정의 및 초기화
 
 Student::Student(
-    std::string name,
-    std::string grade,
-    std::string className,
-    std::string studentNumber
+    std::string& name,
+    std::string& grade,
+    std::string& className,
+    std::string& studentNumber
 ) : name(name), grade(grade), className(className), studentNumber(studentNumber)
 {
-    studentKey = ++studentIndex;  // 학생 생성될 때 증가 (AUTO_INCREMENT)
+    studentKey = ++studentIndex;  // 학생 생성 시 자동 증가 (AUTO_INCREMENT)
 }
 
-int Student::getKey() const
-{
-    return studentKey;
-}
+int Student::getKey() const { return studentKey; }
 
-void Student::setName(const std::string& name) {
-    this->name = name;
-}
+const std::string& Student::getName() const { return name; }
+const std::string& Student::getGrade() const { return grade; }
+const std::string& Student::getClassName() const { return className; }
+const std::string& Student::getStudentNumber() const { return studentNumber; }
 
-void Student::setGrade(const std::string& grade) {
-    this->grade = grade;
-}
-
-void Student::setClassName(const std::string& className) {
-    this->className = className;
-}
-
-void Student::setStudentNumber(const std::string& studentNumber) {
-    this->studentNumber = studentNumber;
-}
-
-void Student::setStudent(
-    const std::string& name,
-    const std::string& grade,
-    const std::string& className,
-    const std::string& studentNumber
-) {
-    setName(name);
-    setGrade(grade);
-    setClassName(className);
-    setStudentNumber(studentNumber);
-}
+void Student::setName(const std::string& name) { this->name = name; }
+void Student::setGrade(const std::string& grade) { this->grade = grade; }
+void Student::setClassName(const std::string& className) { this->className = className; }
+void Student::setStudentNumber(const std::string& studentNumber) { this->studentNumber = studentNumber; }
 
 void Student::setStudent(const Student& updateStudent) {
-    setStudent(
-        updateStudent.name,
-        updateStudent.grade,
-        updateStudent.className,
-        updateStudent.studentNumber
-    ); 
+    name = updateStudent.name;
+    grade = updateStudent.grade;
+    className = updateStudent.className;
+    studentNumber = updateStudent.studentNumber;
+}
+
+std::string Student::getListKey() const {
+    std::ostringstream oss;
+    oss << grade << "-" << className << "-" << studentNumber;
+    return oss.str();
+}
+
+bool Student::isSameStudentInfo(const Student& compareStudent) const
+{
+    return (this->grade == compareStudent.grade) &&
+        (this->className == compareStudent.className) &&
+		(this->studentNumber == compareStudent.studentNumber);
 }
--- student.h
+++ student.h
@@ -4,35 +4,38 @@
 class Student
 {
 private:
-    int studentKey = 0;
-
-public:
-    static int studentIndex;
-
+    int studentKey = -1; // 기본값
     std::string name;
     std::string grade;
     std::string className;
     std::string studentNumber;
 
+public:
+    static int studentIndex; // 학생 키 인덱스
+
     Student() = default;
     Student(
-        std::string name,
-        std::string grade,
-        std::string className,
-        std::string studentNumber
+        std::string& name,
+        std::string& grade,
+        std::string& className,
+        std::string& studentNumber
     );
 
     int getKey() const;
+
+    // Getter
+    const std::string& getName() const;
+    const std::string& getGrade() const;
+    const std::string& getClassName() const;
+    const std::string& getStudentNumber() const;
+	
+    // Setter
     void setName(const std::string& name);
     void setGrade(const std::string& grade);
     void setClassName(const std::string& className);
     void setStudentNumber(const std::string& studentNumber);
 
-    void setStudent(
-        const std::string& name,
-        const std::string& grade,
-        const std::string& className,
-        const std::string& studentNumber
-    );
-    void Student::setStudent(const Student& updateStudent);
+    void setStudent(const Student& updateStudent);
+	std::string getListKey() const;
+    bool isSameStudentInfo(const Student& compareStudent) const;
 };
